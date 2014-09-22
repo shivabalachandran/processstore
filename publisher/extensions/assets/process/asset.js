2096af7a969ff076405b22f53ac298e12730fba7
Shiva Balachandran <shiva@wso2.com>
2014-09-22 23:40:32 +0530

    working association

--- publisher/extensions/assets/process/asset.js
+++ publisher/extensions/assets/process/asset.js
@@ -10,6 +10,7 @@ asset.manager = function(ctx) {
 
             log.info("Entered");
             var ref = require('utils').time;
+            var GovernanceConstants=Packages.org.wso2.carbon.governance.api.util;
             //Check if the options object has a createdtime attribute and populate it 
             if ((options.attributes) && (options.attributes.hasOwnProperty('overview_createdtime'))) {
                 options.attributes.overview_createdtime = ref.getCurrentTime();
@@ -19,53 +20,60 @@ asset.manager = function(ctx) {
             // log.info(options);
             var asset = this.get(options.id);
             log.info(asset);
+            for(var key in this.registry){
+                log.info('key: '+key);
+            }
             //TODO
-            `
+
             //Adding Associatin for Predecessors
             var tempArray1 = asset.attributes.properties_predecessors.split("\,");
 
             for (var i = 0; i < tempArray1.length; i++) {
 
-                    var preAsset = this.get(tempArray1[i]);
-                this.registry.addAssociation(asset.path, preAsset.path, "Predecessors");
-                
-            };
+                var preAsset = this.get(tempArray1[i]);
+                this.registry.associate(asset.path, preAsset.path, GovernanceConstants.USED_BY);
+                log.info("associations Predecessors");
 
-             //Adding Associatin for properties_specializations
+            }
+
+            //Adding Associatin for properties_specializations
             var tempArray2 = asset.attributes.properties_specializations.split("\,");
 
             for (var i = 0; i < tempArray2.length; i++) {
 
-                    var specAsset = this.get(tempArray2[i]);
-                this.registry.addAssociation(asset.path, specAsset.path, "Specializations");
-                
-            };
+                var specAsset = this.get(tempArray2[i]);
+                this.registry.associate(asset.path, specAsset.path, GovernanceConstants.DEPENDS);
+                log.info("associations Successors");
+
+            }
 
 
             //Adding Associatin for properties_generalizations
             var tempArray3 = asset.attributes.properties_generalizations.split("\,");
 
-            for (var i = 0; i < tempArray.length; i++) {
+            for (var i = 0; i < tempArray3.length; i++) {
 
-                    var genAsset = this.get(tempArray3[i]);
-                this.registry.addAssociation(asset.path, genAsset3.path, "Generalizations");
-                
-            };
+                var genAsset = this.get(tempArray3[i]);
+                this.registry.associate(asset.path, genAsset.path, GovernanceConstants.DEPENDS);
+                log.info("associations Generalizations");
 
-                 //Adding Associatin for properties_successors
+            }
+
+            //Adding Associatin for properties_successors
             var tempArray4 = asset.attributes.properties_successors.split("\,");
 
             for (var i = 0; i < tempArray4.length; i++) {
 
-                    var genAsset = this.get(tempArray4[i]);
-                this.registry.addAssociation(asset.path, genAsset.path, "Successors");
-                
-            };
+                var sucAsset = this.get(tempArray4[i]);
+                this.registry.associate(asset.path, sucAsset.path, GovernanceConstants.DEPENDS);
+                log.info("associations Successors");
+
+            }
             /*Get info from the four fields and string split and add associations with special names*/
 
 
 
-            //   this.registry.addAssociation(asset.path, path, GovernanceConstants.USED_BY);;		
+            //   this.registry.addAssociation(asset.path, path, GovernanceConstants.USED_BY);;      
         }
     };
 };
